ae114724462e85a20c998480a4d2bab43d8415b1
Nima <87241816+nimacpp@users.noreply.github.com>
2021-12-26 17:24:01 +0330

    Update Encrypt.rb

--- lib/Encrypt.rb
+++ lib/Encrypt.rb
@@ -1,9 +1,11 @@
 require 'chilkat'
+require_relative 'colorama.rb'
 class Enc
 @ivHex = "000102030405060708090A0B0C0D0E0F"
 @keyHex = "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"
 
 def dec(inFile)
+    if inFile[-1] == 'n'
 decrypt = Chilkat::CkCrypt2.new()
 
 decrypt.put_CryptAlgorithm("aes")
@@ -14,15 +16,15 @@ decrypt.SetEncodedIV(@ivHex,"hex")
 decrypt.SetEncodedKey(@keyHex,"hex")
 # -----------------------------------
 outFile = inFile[0..-2]
-
 success = decrypt.CkDecryptFile(inFile,outFile)
-
 File.delete(inFile) if File.exist?(inFile)
 if (success == false)
-    print decrypt.lastErrorText() + "\n";
-    exit
+    print decrypt.lastErrorText() + "\n".red;
+else
+    puts "[+] success ... complete #{inFile}".green
 end
-
-print "Success!" + "\n";
+else
+    puts "[-] Error encrypt ... #{inFile}".red
+end
 end
 end
